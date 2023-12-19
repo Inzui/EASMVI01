3f9affcd54c7a8c01ebd5c7d77cb42dad8639f1d
Ian Zuiderent <ian@peitsman.com>
2023-12-19 16:04:17 +0100

    Webcam can now be used as input as well

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from photoProcessor import PhotoProcessor
 from dataSetService import DataSetService
 from classifier import Classifier
-import os
+from datetime import datetime
+import os, cv2
+
+FRAMES_PER_SECOND = 2
 
 class Main():
     def __init__(self, identifiersDir: str) -> None:
@@ -20,6 +23,28 @@ class Main():
         if (forceConvert or not self.testDataSet.exists()):
             print("Test CSV does not exist.")
             self._picturesToDataSet(self.testDataSet)
+        
+        # Get pictures from webcam and use as input.
+        print("Loading camera")
+        cam = cv2.VideoCapture(0)
+        lastCaptureTime = datetime.now()
+        while True:
+            _, frame = cam.read()
+            frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
+            cv2.imshow('Input', frame)
+
+            deltaS = (datetime.now() - lastCaptureTime).total_seconds()
+            if (deltaS > 1/FRAMES_PER_SECOND):
+                try:
+                    coordinates = self.photoProcessor.run(frame)
+                except Exception as e:
+                    print(e)
+                finally:
+                    lastCaptureTime = datetime.now()
+
+            c = cv2.waitKey(1)
+            if c == 27:
+                break
     
     def _picturesToDataSet(self, dataSet: DataSetService):
         print(f"Converting pictures from '{dataSet.dataSetType}' to CSV.")
